eecac2dc8dd547faf9efa32568f32fb905d5cb28
Tyler <44188028+twit96@users.noreply.github.com>
2023-01-12 14:08:50 -0600

    Update Pathnames, Header and Footer Code
    Since temp hosting through my GitHub pages, absolute pathnames on scripts breaks. Swap to relative pathnames.
    
    Header and Footer code now uses backtick syntax for multiline strings.

--- includes/footer.js
+++ includes/footer.js
@@ -1,14 +1,12 @@
-document.write('\
-\
-  <footer>\
-    <div class="wrapper">\
-      <b>Juan Gonzalez De Mendoza</b>\
-      <nav>\
-        <a href="https://github.com/JuanGMendoza">GitHub</a>\
-        <a href="https://www.youtube.com/channel/UC560ZaRh8BrtR00J4dH7PgA">YouTube</a>\
-        <a href="mailto:JuanGMendoza@utexas.edu">JuanGMendoza@utexas.edu</a>\
-      </nav>\
-    </div>\
-  </footer>\
-\
-');
+document.write(`
+  <footer>
+    <div class="wrapper">
+      <b>Juan Gonzalez De Mendoza</b>
+      <nav>
+        <a href="https://github.com/JuanGMendoza">GitHub</a>
+        <a href="https://www.youtube.com/channel/UC560ZaRh8BrtR00J4dH7PgA">YouTube</a>
+        <a href="mailto:JuanGMendoza@utexas.edu">JuanGMendoza@utexas.edu</a>
+      </nav>
+    </div>
+  </footer>
+`);
