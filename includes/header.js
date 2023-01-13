3b08b4ecf16ba3fc974b253f45aa110f7cecd734
Tyler <44188028+twit96@users.noreply.github.com>
2023-01-12 20:11:09 -0600

    Blog Page and Blog Post Page Separation

--- includes/header.js
+++ includes/header.js
@@ -23,7 +23,7 @@ document.write(`
             <path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/>
           </svg>
         </button>
-        <a href="/about/">About</a>
+        <a href="/about/">About Me</a>
         <a href="/blog/">Blog</a>
         <a href="/research/">Research</a>
         <a href="/resume/">Resume</a>
@@ -35,7 +35,7 @@ document.write(`
 `);
 
 
-// Header Functionality
+// Add Header Functionality
 var header = document.querySelector("header");
 var hamburger_icon = document.getElementById("hamburger");
 hamburger_icon.onclick = function() {
