954b416159016128c93c8b3f5c171f1da1d8455f
Tyler <44188028+twit96@users.noreply.github.com>
2023-01-13 11:23:00 -0600

    Combined "blog" and "writings" pages

---
+++ includes/blogpost_breadcrumbs.js
@@ -0,0 +1,7 @@
+/* Template Breadcrumbs Element for Blog Post Pages */
+document.write(`
+  <section id="breadcrumbs">
+    <a href="/">Home</a>/
+    <a href="/writings/">Writings</a>/
+  </section>
+`);
